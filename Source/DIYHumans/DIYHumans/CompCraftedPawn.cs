b2421712ff8242e7cfff2e928a9ff60a17a26a59
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-05-31 11:50:49 +0200

    Add files via upload

--- Source/DIYHumans/DIYHumans/CompCraftedPawn.cs
+++ Source/DIYHumans/DIYHumans/CompCraftedPawn.cs
@@ -39,12 +39,54 @@ namespace DIYHumans
             pawnToSpawn.apparel.DestroyAll();
             IntVec3 spawnPosition = this.parent.Position;
             Map spawnMap = this.parent.Map;
-            this.parent.Destroy(DestroyMode.Vanish);
             GenSpawn.Spawn(pawnToSpawn, spawnPosition, spawnMap, WipeMode.Vanish);
-            if (!this.Props.possibleMentalStates.NullOrEmpty() && Rand.Value < this.Props.mentalStateChance)
+            ApplyQualityEffects(pawnToSpawn);
+            ApplyMentalState(pawnToSpawn);
+            this.parent.Destroy(DestroyMode.Vanish);
+        }
+
+        /// <summary>
+        /// applies various effects to the given pawn based on the quality of this thing
+        /// </summary>
+        public void ApplyQualityEffects(Pawn pawn)
+        {
+            CompQuality compQuality = this.parent.TryGetComp<CompQuality>();
+            if (compQuality != null)
+            {
+                QualityCategory quality = compQuality.Quality;
+                //multiplies all skills
+                float skillMultiplier = ((int)quality + 2f) / 4f;
+                if (pawn.skills != null)
+                {
+                    foreach (SkillRecord skill in pawn.skills.skills)
+                    {
+                        skill.Level = (int)(skill.Level * skillMultiplier);
+                    }
+                }
+                //applies infections
+                float infectionChance = (6f - (int)quality) / 12f;
+                if (Rand.Value < infectionChance)
+                {
+                    IEnumerable<BodyPartRecord> bodyParts = pawn.health?.hediffSet?.GetNotMissingParts(BodyPartHeight.Undefined, BodyPartDepth.Undefined, null, null);
+                    int infectionCount = Rand.Range(1, 6 - (int)quality);
+                    for (int i = 0; i < infectionCount; i++)
+                    {
+                        BodyPartRecord randomPart = bodyParts.RandomElement();
+                        pawn.health?.AddHediff(HediffDefOf.WoundInfection, randomPart, null, null);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// applies a random mental state to the given pawn if possible
+        /// </summary>
+        public void ApplyMentalState(Pawn pawn)
+        {
+            if (!!this.Props.possibleMentalStates.NullOrEmpty() && Rand.Value < this.Props.mentalStateChance)
             {
                 MentalStateDef mentalState = this.Props.possibleMentalStates.RandomElement();
-                pawnToSpawn.mindState.mentalStateHandler.TryStartMentalState(mentalState, null, false, false, null, false);
+                pawn.mindState.mentalStateHandler.TryStartMentalState(mentalState, null, false, false, null, false);
             }
         }
     }
